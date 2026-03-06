f93a9b12293cdbdd1c178fea3930158253f2f307
WizardsBowl <125746317+WizardsBowl@users.noreply.github.com>
2026-03-07 01:17:01 +0800

    site: 添加页面底部版权声明

--- docs/.vitepress/shared/index.ts
+++ docs/.vitepress/shared/index.ts
@@ -1,2 +1,6 @@
 export const hostname = 'https://www.wzb233.com';
 export const ogDefaultImage = 'https://img.wizardsbowl.com/const/blog/og-image-fuwanko-1.jpg';
+
+export function getUrlByRelativePath(relativePath: string): string {
+  return `${hostname}${relativePath}`;
+}

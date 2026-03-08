a218ee34a0905f992ae176fcda5c68ff3600f3df
WizardsBowl <125746317+WizardsBowl@users.noreply.github.com>
2026-03-08 12:52:42 +0800

    chore: 将网站从www域迁移到blog域

--- docs/.vitepress/shared/siteInfo.ts
+++ docs/.vitepress/shared/siteInfo.ts
@@ -1,6 +1,6 @@
 import type { PageData } from 'vitepress';
 
-export const hostname = 'https://www.wzb233.com';
+export const hostname = 'https://blog.wzb233.com';
 export const ogDefaultImage = 'https://img.wizardsbowl.com/const/blog/og-image-fuwanko-1.jpg';
 
 export function getUrlByPageData(page: PageData): string {

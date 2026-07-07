99306b815d50ed55ec70beeb23475239bd3ca739
WizardsBowl <125746317+WizardsBowl@users.noreply.github.com>
2026-07-07 17:02:40 +0800

    site: 更改默认文章封面图

--- docs/.vitepress/shared/siteInfo.ts
+++ docs/.vitepress/shared/siteInfo.ts
@@ -1,7 +1,7 @@
 import type { PageData } from 'vitepress';
 
 export const hostname = 'https://blog.wzb233.com';
-export const defaultCoverImage = 'https://img.wizardsbowl.com/const/blog/og-image-fuwanko-1.jpg';
+export const defaultCoverImage = 'https://img.wizardsbowl.com/const/blog/og-image-fuwanko-1-1.jpg';
 
 export function getUrlByPageData(page: PageData): string {
   return `${hostname}/${page.relativePath.replace(/\.md$/, '')}`;
